8c741be0024795bd62b1a6759b162381efa04409
brunopaiva1 <brunopaivamaster10@gmail.com>
2023-02-26 16:26:25 -0300

    3 commit

--- Histograma.cpp
+++ Histograma.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 #define NUM_BINS 6
-#define MIN_VALUE 0
-#define MAX_VALUE 6
+#define min_meas 0
+#define max_meas 6
 
 int main() {
     int bin_count[NUM_BINS] = {0};
     int bin_maxes[NUM_BINS] = {0};
-    int bin_width = (MAX_VALUE - MIN_VALUE) / NUM_BINS;
+    int bin_width = (max_meas - min_meas) / NUM_BINS;
     int data_count;
     int i, j;
 
@@ -23,18 +23,18 @@ int main() {
 
     // contar os valores em cada bin
     for (i = 0; i < data_count; i++) {
-        int bin = (values[i] - MIN_VALUE) / bin_width;
+        int bin = (values[i] - min_meas) / bin_width;
         bin_count[bin]++;
     }
 
     // calcular o máximo de cada bin
     for (i = 0; i < NUM_BINS; i++) {
-        bin_maxes[i] = MIN_VALUE + bin_width * (i + 1);
+        bin_maxes[i] = min_meas + bin_width * (i + 1);
     }
 
     // imprimir o histograma
     for (i = 0; i < NUM_BINS; i++) {
-        printf("%2d-%2d | ", MIN_VALUE + i*bin_width, MIN_VALUE + (i+1)*bin_width - 1);
+        printf("%2d-%2d | ", min_meas + i*bin_width, min_meas + (i+1)*bin_width - 1);
         for (j = 0; j < bin_count[i]; j++) {
             printf("*");
         }
